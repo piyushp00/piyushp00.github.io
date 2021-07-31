6585be9d898be2becd7449b06ab9a07a29c20c61
Piyush Pandey <piyushpvns@gmail.com>
2021-07-31 15:04:58 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -147,7 +147,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`app-a78f74292ee1b92d54ca.js`))) {
+  if (!resources || !(await caches.match(`/app-a78f74292ee1b92d54ca.js`))) {
     return await fetch(event.request)
   }
 
